4bda7b63b535ead59c88a93a6616f9a8673f1e8c
Fabricio Cabral <fabriciofx@gmail.com>
2016-01-24 20:19:45 -0300

    Indent correction

--- FileContent.java
+++ FileContent.java
@@ -4,12 +4,15 @@ import java.nio.file.Files;
 
 public final class FileContent implements Content {
   private final File file;
+
   public FileContent(final File file) {
     this.file = file;
   }
+
   public String data() throws IOException {
     return new String(Files.readAllBytes(file.toPath()));
   }
+
   public void save(final String data) throws IOException {
     Files.write(file.toPath(), data.getBytes());
   }
--- NonUnicode.java
+++ NonUnicode.java
@@ -2,12 +2,15 @@ import java.io.IOException;
 
 public final class NonUnicode implements Content {
   private final Content origin;
+
   public NonUnicode(final Content content) {
     this.origin = content;
   }
+
   public String data() throws IOException {
     return origin.data().replaceAll("\\P{Print}", "");
   }
+
   public void save(final String data) throws IOException {
     origin.save(data);
   }
